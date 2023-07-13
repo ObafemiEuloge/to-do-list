abdbd53471cebc20d24068e77276c20267d3630e
AGNOUN Obafèmi Euloge <128822973+ObafemiEuloge@users.noreply.github.com>
2023-07-13 02:16:22 +0200

    msg

--- js/script.js
+++ js/script.js
@@ -1,97 +1,46 @@
 `use strict`
 
-document.addEventListener("DOMContentLoaded", () => {
-    let input = document.getElementById("list");
+document.addEventListener("DOMContentLoaded", function(){
+/**
+ * RECUPERATION DES VARIABLES DU DOM
+ */
+    let input = document.querySelector("#list");
+    let btnActive = document.querySelector("active");
+    let ul = document.querySelector("ul")
+    let btnComplete = document.querySelector("complete");
+    let btnAll = document.querySelector("all");
+    let btnClearAll = document.querySelector("clearAll");
+    let tabTask = [];
 
+    /**
+     * LES FONCTIONS
+     */
 
-    class List {
-        // constructor (element){
-        //     this.element = element;
+    function creatTask (){
+        let li = document.createElement("li");
+        ul.appendChild(li);
+        let liTable = input.value;
+        tabTask.push(liTable);
+        let tableList = tabTask[tabTask.length - 1];
+        li.innerHTML = "<span class='circle'></span>"
+        li.innerHTML += tableList;
+        li.innerHTML += "<span>x</span>";
+        
 
-        // }
+        
 
-        enterElement() {
-            document.addEventListener("keydown", (e) => {
-                if (e.key === "Enter") {
-                    let element = document.createElement("li");
-                    element.classList.add("element");
-                    document.querySelector("main > ul").appendChild(element);
-                    element.innerHTML = '<button class="checkbox">&#10004;</button>';
-                    element.innerHTML += `<p class ="valeur">${input.value}</p>`;
-                    element.innerHTML += '<span class="removeElement">&#10006</span>';
-                    input.value = "";
-                    input.innerHTML = input.value;
-                    // tabList.push(element);                
+    }   
 
-                    let span = document.querySelectorAll(".element span.removeElement");
-                        console.log(span.parentElement);
-                        for (let i = 0; i < span.length; i++) {
-                            span[i].addEventListener("click", function() {
-                                let removes = this.parentElement;
-                                // console.log(removes);
-                                removes.remove();
-                            });
-                        }
-                        let checkbox = document.querySelectorAll("button.checkbox");
-                        for (let j = 0; j < checkbox.length; j++) {
-                            checkbox[j].addEventListener("click", function() {
-                                let done = document.querySelectorAll(".element p");
-                                // console.log(done);
-                                done[j].style.textDecoration = "line-through";
-                            });
-                        }
-
-                        let all = document.querySelector("#all");
-                        let active = document.querySelector("#active");
-                        let completed = document.querySelector("#completed");
-                        let clearAll = document.querySelector("#clearAll");
-
-
-                        function clearAlls() {
-                            element.remove();
-                        }
-
-                        function completeds() {
-                            if(done[j].style.textDecoration !== "line-through"){
-                                element.style.display = "none";
-                            }
-                        }
-                        function actives() {
-                            if(done[j].style.textDecoration === "line-through"){
-                                element.style.display = "none";
-                            }
-                        }
-
-                        function alls() {
-                            element;
-                        }
-
-                        all.addEventListener("click", alls)
-                        clearAll.addEventListener("click", clearAlls)
-                        completed.addEventListener("click", completeds)
-                        active.addEventListener("click", actives)
-                        
-                }
-                
-
-
-
-
-
-
-
-            });
-
-
-
-
+    input.addEventListener("keydown", (e)=>{
+        console.log(e.key)
+        if (e.key === "Enter") {
+            creatTask();
+            input.value = "";
         }
+    })
+
+    function addTask () {
+        
+        
     }
-
-    let element = new List();
-    element.enterElement();
-    // element.doneElement();
-
-
-
 });
